660f7b54bdb9ac68ffc7416f7306e7c197a36af5
Jouusey <josevitor110m.p@gmail.com>
2025-02-04 16:08:59 -0300

    Criação Projeto P

---
+++ projectP/src/pages/LandingPage.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./LandingPage.css"
+import Header from '../components/Header'
+
+function LandingPage() {
+  return (
+
+    
+    <div className="landingPage-container">
+        <Header />
+   <img src="./public/images/2lvto440juw21.jpg" class="bg"></img>
+    </div>
+  )
+}
+
+export default LandingPage
