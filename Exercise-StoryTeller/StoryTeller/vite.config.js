3267ba3db6db8ff0af88b52da35ba644980cc171
jouusey <josevitor110m.p@gmail.com>
2025-02-18 16:44:35 -0300

    update

--- Exercise-StoryTeller/StoryTeller/vite.config.js
+++ Exercise-StoryTeller/StoryTeller/vite.config.js
@@ -1,7 +1,30 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'StoryTeller',
+        short_name: 'ST',
+        description: 'Desbrave uma nova história!',
+        theme_color: '***#ffffff',
+        icons: [
+          {
+            src: 'node-192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'node-512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
+});
