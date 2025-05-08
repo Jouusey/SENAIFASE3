685e2441f1e047a78e2ba13e45be1acfab2c990b
jouusey <josevitor110m.p@gmail.com>
2025-05-08 16:50:54 -0300

    coisa

---
+++ testeUnit/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <div>
+
+<h1>OLÁ</h1>
+
+<h3>HELP HELP</h3>
+
+    </div>
+    </>
+  )
+}
+
+export default App
